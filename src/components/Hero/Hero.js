8389eb36bb136707d61602e50bf8b441f38686af
Kamila Gajcy <kamila.gajcy@gmail.com>
2021-09-05 19:35:14 +0200

    just start

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Hero.scss';
+
+function Hero() {
+    return (
+      <header className ={styles.component}>
+        <h2 className={styles.title}>Things to do</h2>
+        <img className={styles.title} src></img>
+      </header>
+    )
+  }
+
+export default Hero;
